d9c9908c4ec9919c5a9e1465be2ff729245f4858
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-04-23 01:55:33 -0700

    Only Question 7 left

--- Question no.6/question6.js
+++ Question no.6/question6.js
@@ -2,7 +2,7 @@ document.write(
   "<h1>Write a program that shows a random number between 1 <br> and 100 in your browser.</h1>"
 );
 
-var randomS = Math.floor(Math.random() * 100);
+var randomS = Math.floor(Math.random() * 100) + 1;
 
 document.write(
   "<h2>The Random Number Between 1 and 100 is: " + randomS + "</h2>"
