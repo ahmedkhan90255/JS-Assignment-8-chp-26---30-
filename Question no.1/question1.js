a2e50ad0c9c7d76c6d83ba847b5c3045241cd36b
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-04-23 00:03:00 -0700

    Question1 completed

---
+++ Question no.1/question1.js
@@ -0,0 +1,16 @@
+document.write(
+  "<h1>Write a program that takes a positive integer from user & <br> display the following in your browser. <br>a)Number <br>b)Roundoff Value <br>c)Ceil Value <br>d)Floor Value</h1>"
+);
+
+var userInput = Number(prompt("Enter Any Number"));
+
+document.write("<h2>Number: " + userInput + "</h2>");
+
+var userInput2 = Math.round(userInput);
+document.write("<h2>Roundoff Value: " + userInput2 + "</h2>");
+
+var userInput3 = Math.ceil(userInput);
+document.write("<h2>Ceil Value: " + userInput3 + "</h2>");
+
+var userInput4 = Math.floor(userInput);
+document.write("<h2>Floor Value: " + userInput4 + "</h2>");
